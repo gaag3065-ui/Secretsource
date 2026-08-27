fc69b3a1f05ea71e20dd7207c1aa8acd30515ddb
gaag3065-ui <296028315+gaag3065-ui@users.noreply.github.com>
2026-08-27 09:39:33 +0700

    chore(config): update backend tunnel endpoint

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const LOCAL_API_URL = 'http://127.0.0.1:43127';
 
 // Backend สำหรับเว็บไซต์จริง
 // ตอนนี้ยังใช้ค่าว่างไว้ก่อน จนกว่าเราจะจัดการ Production Backend
-const PRODUCTION_API_URL = 'https://methods-minister-spin-lighter.trycloudflare.com';
+const PRODUCTION_API_URL = 'https://identical-song-explosion-recycling.trycloudflare.com';
 
 
 // API หลักที่ทุกหน้าในระบบจะเรียกใช้
