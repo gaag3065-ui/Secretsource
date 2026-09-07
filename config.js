59a60ba774363d2c5839cb729fcdba43934bb5e0
gaag3065 <296028315+gaag3065-ui@users.noreply.github.com>
2026-09-07 21:41:59 +0700

    chore: sync Cloudflare tunnel URL (automated)

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ const LOCAL_API_URL = `http://127.0.0.1:${localApiPort}`;
 
 
 
-const PRODUCTION_API_URL = 'https://space-add-lovely-peaceful.trycloudflare.com';
+const PRODUCTION_API_URL = 'https://platforms-complications-pounds-series.trycloudflare.com';
 
 
 
@@ -214,6 +214,7 @@ window.performSecureLogout =
         }
     };
 //#endregion
+
 
 
 
